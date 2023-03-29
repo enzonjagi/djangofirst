99ade8b5ea95d355a70ace81f61d55c425eaf147
enzonjagi <amosndungo@gmail.com>
2023-03-29 15:26:18 +0300

    Created a new superuser and allowed the poll app to be modifiable on the admin panel - The db file not included

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Question
+
+"""
+    Makes the poll app modifiable in the admin site
+"""
 
 # Register your models here.
+admin.site.register(Question)

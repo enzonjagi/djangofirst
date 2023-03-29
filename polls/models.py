22e3c7f4dea2ddd9eb76745224b4a86c05375433
enzonjagi <amosndungo@gmail.com>
2023-03-29 15:16:34 +0300

    Added code to: 1. Create better output when calling the text, 2. Check if the question was published recently

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,8 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 """We'll be defining models in this file.
@@ -13,10 +17,24 @@ class Question(models.Model):
     Attributes:
         - question_text: the question itself
         - pub_date: the publishing date for the question
+    Methods:
+        - __str__ : adding a clean representation of the class
+        - was_published_recently: Should check and show if the
+                                    question was published recently
     """
 
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        """Should return the question_text when called."""
+
+        return self.question_text
+    
+    def was_published_recently(self):
+        """Should check and show if the question was published recently."""
+
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -26,8 +44,15 @@ class Choice(models.Model):
         - question: refers to the question that was asked(a foreign key)
         - choice_text: the text for each individual choice given for the question
         - votes: the number of votes allocated to the above choice
+    Methods:
+        - __str__ : adding a clean representation of the class
     """
 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        """Should return the choice_text when called."""
+        
+        return self.choice_text

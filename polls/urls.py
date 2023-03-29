f6ef8c3f3279b6cd63a590ec9dfb30a6b92f3b18
enzonjagi <amosndungo@gmail.com>
2023-03-29 17:14:43 +0300

    Removed the hardcoded URL in the index template and added a namespace 'polls' to the polls app for referencing in the templates and views

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+# define the app name for namespacing reasons
+app_name = 'polls'
+
 # here's where the url patterns for the views will be created 
 urlpatterns = [
     path('', views.index, name='index'),

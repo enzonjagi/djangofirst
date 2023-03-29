4313b1ad8239ba39e2818fe9882e2bd4422f6613
enzonjagi <amosndungo@gmail.com>
2023-03-29 16:52:51 +0300

    The first attempt at showing the Question views on the polls app index page using templates

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 def index(request):
@@ -8,7 +11,14 @@ def index(request):
     Attributes:
         - request: not used yet
     """
-    return HttpResponse("Salaam peeps. Welcome to the Polls index page")
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     """Dispays a question in detail

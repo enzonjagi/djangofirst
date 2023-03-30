7459b78b39223c4af633238bf233c94fd2b7020d
enzonjagi <amosndungo@gmail.com>
2023-03-30 10:04:42 +0300

    Updated the details view to post the selected choice to the local db

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -66,4 +67,26 @@ def vote(request, question_id):
         - question_id: the id of the question
     """
 
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        # Redisplay the question for voting
+        return render(
+            request,
+            'polls/detail.html',
+            {
+                'question': question,
+                'error_message': "Please pick one of the choices",
+            }
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # return a redirect after posting data to avoid posting it twice
+        return HttpResponseRedirect(
+            reverse('polls:results', args=(question_id,))
+        )
+
+    # return HttpResponse("You're voting on question %s." % question_id)

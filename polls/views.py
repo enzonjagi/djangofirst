a35393970495857e7020cf870ffdd7fb7be058d3
enzonjagi <amosndungo@gmail.com>
2023-03-30 10:16:18 +0300

    Updated the results view to show how many votes each choice received

--- polls/views.py
+++ polls/views.py
@@ -54,9 +54,15 @@ def results(request, question_id):
         - question_id: the id of the question
     """
 
-    response = "You're looking at the results of question %s."
-    
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(
+        request,
+        'polls/results.html',
+        {
+            'question': question
+        }
+    )
 
 
 def vote(request, question_id):

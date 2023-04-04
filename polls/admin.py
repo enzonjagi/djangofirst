517189a6e8c8d68486cd1a619b8c97c82d9b1424
enzonjagi <amosndungo@gmail.com>
2023-04-04 17:48:39 +0300

    Admin should now be able to add a questions from the adminpanel

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 """
     Makes the poll app modifiable in the admin site
 """
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {
+            'fields': ['question_text']
+        }),
+        ('Date information', {
+            'fields': ['pub_date']
+        }),
+    ]
+    inlines = [ChoiceInline]
+    list_display = (
+        'question_text',
+        'pub_date',
+        'was_published_recently'
+    )
+
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
